1e4ef162a13f799297f530e18e10a3c1d7c4b116
ishraque-dev <ahamedesrak3@gmail.com>
2023-03-07 12:34:35 +0600

    fixed deploy issue

--- src/components/UI/SmallCard.jsx
+++ src/components/UI/SmallCard.jsx
@@ -3,7 +3,7 @@ export default function SmallCard({ img, location, distance }) {
   return (
     <div className="m-2 mt-4 flex transform cursor-pointer items-center space-x-3 rounded-xl transition duration-200 hover:scale-105 hover:bg-gray-100">
       <div className="relative h-16 w-16 ease-out ">
-        <Image src={img} layout="fill" className="rounded-lg" />
+        <Image src={img} layout="fill" className="rounded-lg" alt="small" />
       </div>
       <div>
         <h2>{location}</h2>
